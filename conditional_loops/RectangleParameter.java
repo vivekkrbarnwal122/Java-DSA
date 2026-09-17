f8255e2b3d8b46a1d07baf8eef633c5f14acbe89
VIVEK KUMAR <vivekkumarbarnwal72560@gmail.com>
2026-09-17 07:31:39 +0530

    parameter of rectangle

---
+++ conditional_loops/RectangleParameter.java
@@ -0,0 +1,14 @@
+ import java.util.Scanner;
+   public class RectangleParameter {
+    public static void main(String[] args){
+        System.out.println("enter length and width of rectangle: ");
+        Scanner in =new Scanner(System.in);
+        int L= in.nextInt();
+        int W= in.nextInt();
+        double perimeter=2*(L+W);
+        System.out.println("Perimeter of Rectangle : "+perimeter);
+
+    }
+}
+ 
+
