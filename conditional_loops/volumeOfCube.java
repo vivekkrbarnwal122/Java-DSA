22b491f36755d5575183b914a50f18bd76e6a44a
VIVEK KUMAR <vivekkumarbarnwal72560@gmail.com>
2026-09-17 08:22:11 +0530

    volume of cone

---
+++ conditional_loops/volumeOfCube.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class volumeOfCube {
+        public static void main(String[] args){
+        System.out.println("enter height and radius of cone : ");
+        Scanner in =new Scanner(System.in);
+
+        int h = in.nextInt();
+        int r = in.nextInt();
+        double volume=(.03)*3.14*r*r*h;
+        System.out.println("volume of cone  : "+volume);
+
+    }
+}
+ 
+
+
+
+
+
+ 
+
